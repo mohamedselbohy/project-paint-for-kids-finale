1ebf5fc9b2002bb45f8a207a36647f9f851cd50e
mohamedselbohy <msayedelbohy@hotmail.com>
2022-12-23 15:34:43 +0200

    added the ability to change colors of an existing figure

--- FillClraction.cpp
+++ FillClraction.cpp
@@ -20,37 +20,77 @@ void FillClrAction::Execute()
 {
 	ReadActionParameters();
 	Output* pOut = pManager->GetOutput();
-	switch (coloract)
+	if (pManager->GetSelectedFig() == NULL)
 	{
-	case COLOR_BLACK:
-		UI.FillColor = BLACK;
-		pOut->PrintMessage("Black it is ! let's go draw some black stuff!!");
-		break;
-	case COLOR_BLUE:
-		UI.FillColor = BLUE;
-		pOut->PrintMessage("Blue it is ! let's go draw some blue stuff!!");
-		break;
-	case COLOR_GREEN:
-		UI.FillColor = GREEN;
-		pOut->PrintMessage("Green it is ! let's go draw some green stuff!!");
-		break;
-	case COLOR_ORANGE:
-		UI.FillColor = ORANGE;
-		pOut->PrintMessage("Orange it is ! let's go draw some orange stuff!!");
-		break;
-	case COLOR_RED:
-		UI.FillColor = RED;
-		pOut->PrintMessage("Red it is ! let's go draw some red stuff!!");
-		break;
-	case COLOR_YELLOW:
-		UI.FillColor = YELLOW;
-		pOut->PrintMessage("Yellow it is ! let's go draw some yellow stuff!!");
-		break;
-	case DRAW_FILLING_COLOR:
-		UI.FillColor = LIGHTGOLDENRODYELLOW;
-		pOut->PrintMessage("No fill! let's draw them empty!!");
-		break;
-	default:
-		pOut->PrintMessage("That's not a color :(");
+		switch (coloract)
+		{
+		case COLOR_BLACK:
+			UI.FillColor = BLACK;
+			pOut->PrintMessage("Black it is ! let's go draw some black stuff!!");
+			break;
+		case COLOR_BLUE:
+			UI.FillColor = BLUE;
+			pOut->PrintMessage("Blue it is ! let's go draw some blue stuff!!");
+			break;
+		case COLOR_GREEN:
+			UI.FillColor = GREEN;
+			pOut->PrintMessage("Green it is ! let's go draw some green stuff!!");
+			break;
+		case COLOR_ORANGE:
+			UI.FillColor = ORANGE;
+			pOut->PrintMessage("Orange it is ! let's go draw some orange stuff!!");
+			break;
+		case COLOR_RED:
+			UI.FillColor = RED;
+			pOut->PrintMessage("Red it is ! let's go draw some red stuff!!");
+			break;
+		case COLOR_YELLOW:
+			UI.FillColor = YELLOW;
+			pOut->PrintMessage("Yellow it is ! let's go draw some yellow stuff!!");
+			break;
+		case DRAW_FILLING_COLOR:
+			UI.FillColor = LIGHTGOLDENRODYELLOW;
+			pOut->PrintMessage("No fill! let's draw them empty!!");
+			break;
+		default:
+			pOut->PrintMessage("That's not a color :(");
+		}
+	}
+	else
+	{
+		switch (coloract)
+		{
+		case COLOR_BLACK:
+			pManager->GetSelectedFig()->ChngFillClr(BLACK);
+			pOut->PrintMessage("Black it is ! let's go draw some black stuff!!");
+			break;
+		case COLOR_BLUE:
+			pManager->GetSelectedFig()->ChngFillClr(BLUE);
+			pOut->PrintMessage("Blue it is ! let's go draw some blue stuff!!");
+			break;
+		case COLOR_GREEN:
+			pManager->GetSelectedFig()->ChngFillClr(GREEN);
+			pOut->PrintMessage("Green it is ! let's go draw some green stuff!!");
+			break;
+		case COLOR_ORANGE:
+			pManager->GetSelectedFig()->ChngFillClr(ORANGE);
+			pOut->PrintMessage("Orange it is ! let's go draw some orange stuff!!");
+			break;
+		case COLOR_RED:
+			pManager->GetSelectedFig()->ChngFillClr(RED);
+			pOut->PrintMessage("Red it is ! let's go draw some red stuff!!");
+			break;
+		case COLOR_YELLOW:
+			pManager->GetSelectedFig()->ChngFillClr(YELLOW);
+			pOut->PrintMessage("Yellow it is ! let's go draw some yellow stuff!!");
+			break;
+		case DRAW_FILLING_COLOR:
+			pManager->GetSelectedFig()->ChngFillClr(LIGHTGOLDENRODYELLOW);
+			pOut->PrintMessage("No fill! let's draw them empty!!");
+			break;
+		default:
+			pOut->PrintMessage("That's not a color :(");
+		}
+		pManager->UpdateInterface();
 	}
 }
